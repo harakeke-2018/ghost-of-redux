64693b3cebf7798ecb2181dade3f0bf6f03d65b3
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-16 11:45:26 +1300

    converted STORY_WORDS back to variable

--- client/reducers/storyWordReducer.js
+++ client/reducers/storyWordReducer.js
@@ -1,11 +1,10 @@
-// import STORY_WORDS from '../actions'
+import {STORY_WORDS} from '../actions'
 
 const initialState = {}
 
 function storyWordReducer (state = initialState, action) {
-  console.log('reducer', action)
   switch (action.type) {
-    case 'STORY_WORDS':
+    case STORY_WORDS:
       return action.storyWords
     default:
       return state
