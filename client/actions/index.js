ed91e46f6a4a2c7125e2f76b72a2b98d87012d00
adam-kuhn <adam.kuhn20@gmail.com>
2018-02-16 15:47:03 +1300

    api call complete

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,6 +1,7 @@
 import request from 'superagent'
 
 export const STORY_WORDS = 'STORY_WORDS'
+export const SURVIVAL = 'SURVIVAL'
 
 export const addWord = (word) => {
   return {
@@ -26,17 +27,28 @@ export const storyWords = (words) => {
   }
 }
 
-//async
+export const survivalRate = (percent) => {
+  return {
+    type: SURVIVAL,
+    survive: percent
+  }
+}
+
+// async
 
 export const beginStory = (words) => {
   return (dispatch) => {
     dispatch(storyWords(words))
     request
-    .get(`https://love-calculator.p.mashape.com/getPercentage?fname=${words.name1}&sname=${words.name2}`)
-    .set('X-Mashape-Key', '')
-    .set("X-Mashape-Host", "love-calculator.p.mashape.com")
-    .end(function (result) {
-      console.log(result.status, result.headers, result.body)
-    })
+      .get(`https://love-calculator.p.mashape.com/getPercentage?fname=${words.name1}&sname=${words.name2}`)
+      .set('X-Mashape-Key', 'pFxXBOWpOcmshn2bxBavtOparZrQp12vpZajsnxjT7EXRmViHZ')
+      .set('X-Mashape-Host', 'love-calculator.p.mashape.com')
+      .end((err, result) => {
+        if (err) {
+          console.error(err.message)
+        }
+        console.log(result.status, result.headers, result.body.percentage)
+        dispatch(survivalRate(result.body.percentage))
+      })
   }
 }
