6a395ad63f3398bbeab4013a8319ee93af4c9a8a
adam-kuhn <adam.kuhn20@gmail.com>
2018-02-16 17:02:05 +1300

    %and door content up to date

--- client/components/Door.jsx
+++ client/components/Door.jsx
@@ -9,9 +9,9 @@ class Door extends React.Component {
       showResults: false
     }
     this.handleClickDoor = this.handleClickDoor.bind(this)
-  // }
-  // handleClick (e) {
-  //   this.props.dispatch(showComponent(e.target.id))
+    // }
+    // handleClick (e) {
+    //   this.props.dispatch(showComponent(e.target.id))
   }
 
   handleClickDoor () {
@@ -22,21 +22,27 @@ class Door extends React.Component {
 
   render () {
     return (
-      <div>
-        <h1>You chose the door</h1>
-        <p> You thought of your friend <strong>{this.props.friend}</strong>, who would open the door.
-          <br />
-        The <strong>{this.props.smell}</strong> smell was getting worse! </p>
-
-        <button onClick={this.handleClickDoor}>Open the door</button>
-
-        {this.state.showResults &&
-        <div>
-          <p>Behind the door was a <strong>{this.props.animal}</strong>.
-            <br />
-        The <strong>{this.props.animal}</strong> attacks you and you die a slow painful death.</p>
-          <a href='/'>Try again!</a>
+      <div className='container'>
+        <h1 className='title row'>You chose the door</h1>
+        <div className='row'>
+          <p>They walk up to the house, and <strong>{this.props.words.adjective2} {this.props.words.name1}</strong>, who insisted on opening the door, walks up and knocks twice.
+            <br />There is a strong <strong>{this.props.words.smell1}</strong> smell which gets worse as they get closer!
+            <br /> The door creaks open...</p>
         </div>
+        <div className='row'>
+          <button onClick={this.handleClickDoor}>Open the door</button>
+        </div>
+        {this.state.showResults &&
+          <div>
+            <div className='row'>
+              <p>Behind the door is a <strong>{this.props.words.animal2}</strong>.
+                <br />
+                The <strong>{this.props.words.animal2}</strong> attacks you and you have a {this.props.percent}% chance of dieing a slow painful death. Sorry!</p>
+            </div>
+            <div className='row'>
+              <a href='/'>Try again!</a>
+            </div>
+          </div>
         }
         {/* <button id='window' onClick={this.handleClick}>Show </button> */}
       </div>
@@ -46,9 +52,8 @@ class Door extends React.Component {
 
 function mapStateToProps (state) {
   return {
-    friend: state.storyWordReducer.name2,
-    smell: state.storyWordReducer.smell1,
-    animal: state.storyWordReducer.animal1
+    words: state.storyWordReducer,
+    percent: state.survival
   }
 }
 export default connect(mapStateToProps)(Door)
